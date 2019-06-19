a404115913f03d7b8b1384d0240bfe8c8ccab6bd
Demonkratiy <demonkratiy@gmail.com>
2019-06-19 19:43:58 +0300

    v0.0

---
+++ script.js
@@ -0,0 +1,34 @@
+const searchForm = document.querySelector('#search-form');
+const movie = document.querySelector('#movies')
+function apiSearch(event){
+  event.preventDefault();
+  const searchText = document.querySelector('.form-control').value;
+  const server = 'https://api.themoviedb.org/3/search/multi?api_key=fe1ddbf26c8c5d6d90244797b03f0315&language=ru&query=' + searchText;
+  requestApi('GET', server);
+}
+
+searchForm.addEventListener('submit', apiSearch);
+
+function requestApi(method, url){
+  const request = new XMLHttpRequest();
+  request.open(method, url)
+  request.send();
+  request.addEventListener('readystatechange', function() {
+    if (request.readyState !== 4) return;
+    if(request.status !== 200){
+      console.log('Error: ' + request.status);
+      return;
+    }
+    const output = JSON.parse(request.responseText);
+
+    let inner = '';
+
+    output.results.forEach(function (item){
+      let nameItem = item.name || item.title;
+      //inner += '<div class="col-5">' + nameItem + '</div>';
+      inner += `<div class="col-15 col-md-4 col-xl-3">${nameItem};&#8194</div>`;
+    });
+    movie.innerHTML = inner;
+    console.log(output);
+  });
+}
